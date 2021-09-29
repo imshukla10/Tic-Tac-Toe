a20ac51501c082996bb7f2e166b871c2ce437e84
Ritesh <ritesh.1923ec1099@kiet.edu>
2021-09-29 22:43:30 +0530

    third commit

--- project/TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ project/TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,21 +23,26 @@ class MainActivity : AppCompatActivity() {
                              arrayOf(0,3,6), arrayOf(1,4,7), arrayOf(2,5,8),
                              arrayOf(0,4,8), arrayOf(2,4,6) )
 
-    fun tapTap(view: View) {
+    fun tapTap(view: View)
+    {
         var img: ImageView = view as ImageView
         var tapped = Integer.parseInt(img.getTag().toString())
         if (gamesState[tapped] == 2) {
             gamesState[tapped] == activePlayer
-            im
+            img.translationY = -1000f
+            var status = findViewById<TextView>(R.id.textView2)
             if (activePlayer == 0) {
                 img.setImageResource(R.drawable.x)
                 activePlayer = 1
+                status.setText("O's Turn Tap to Play")
             }
             else {
                 img.setImageResource(R.drawable.o)
                 activePlayer = 0
+                status.setText("X's turn Tap to Play")
             }
+            img.animate().translationYBy(1000f).setDuration(300)
         }
-        img.animate().translationYBy(1000f).setDuration(300)
+
     }
 }
