a043e31bbc832fe1f7fcd60638b847fa61533f5c
Ritesh <ritesh.1923ec1099@kiet.edu>
2021-09-30 21:40:09 +0530

    fourth commit

--- project/TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ project/TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
+    var gameActive:Boolean = true
     //0-X
     //1-O
     var activePlayer=0;
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 //    0-X
 //    1-O
 //    2-NULL
-    var winPosition= arrayOf(arrayOf(0,1,2), arrayOf(3,4,5), arrayOf(6,7,8),
+    var winPositions= arrayOf(arrayOf(0,1,2), arrayOf(3,4,5), arrayOf(6,7,8),
                              arrayOf(0,3,6), arrayOf(1,4,7), arrayOf(2,5,8),
                              arrayOf(0,4,8), arrayOf(2,4,6) )
 
@@ -27,7 +27,10 @@ class MainActivity : AppCompatActivity() {
     {
         var img: ImageView = view as ImageView
         var tapped = Integer.parseInt(img.getTag().toString())
-        if (gamesState[tapped] == 2) {
+        if(!gameActive){
+            gameReset()
+        }
+        if (gamesState[tapped] == 2 && gameActive) {
             gamesState[tapped] == activePlayer
             img.translationY = -1000f
             var status = findViewById<TextView>(R.id.textView2)
@@ -43,6 +46,40 @@ class MainActivity : AppCompatActivity() {
             }
             img.animate().translationYBy(1000f).setDuration(300)
         }
+        for(winPosition in winPositions) {
+            // somebody won we have to find out who win
+            if((gamesState[winPosition[0]]==gamesState[winPosition[1]]) && (gamesState[winPosition[1]]==gamesState[winPosition[2]]) && gamesState[winPosition[0]]!=2)
+            {
+                var winner:String
+                if (gamesState[winPosition[0]]==0){
+                    winner = "X has won"
+                }
+                else{
+                    winner = "O has won"
+                }
+//              Winner anouncement
+                var status = findViewById<TextView>(R.id.textView2)
+                status.setText(winner)
+            }
+
+        }
+    }
+
+    private fun gameReset() {
+        gameActive = true
+        activePlayer = 0
+        for(i in 0..gamesState.size){
+            gamesState[i] = 2
+        }
+        var imageView = findViewById<ImageView>(R.id.imageView1).setImageResource(0)
+        var imageView2 = findViewById<ImageView>(R.id.imageView2).setImageResource(0)
+        var imageView3 = findViewById<ImageView>(R.id.imageView3).setImageResource(0)
+        var imageView4 = findViewById<ImageView>(R.id.imageView4).setImageResource(0)
+        var imageView5 = findViewById<ImageView>(R.id.imageView5).setImageResource(0)
+        var imageView6 = findViewById<ImageView>(R.id.imageView6).setImageResource(0)
+        var imageView7 = findViewById<ImageView>(R.id.imageView7).setImageResource(0)
+        var imageView8 = findViewById<ImageView>(R.id.imageView8).setImageResource(0)
+        var imageView9 = findViewById<ImageView>(R.id.imageView9).setImageResource(0)
 
     }
 }
